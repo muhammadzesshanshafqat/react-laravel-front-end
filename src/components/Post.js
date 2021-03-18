f0d7585a334f0b661a7d78a07b3da9f4fc478c5f
Muhammad Zeeshan Shafqat <zeeshan_github@outlook.com>
2021-03-19 05:55:36 +1100

    added post component for displaying post

---
+++ src/components/Post.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: this.props.post.id,
+            userId: this.props.post.user_id,
+            postTitle: this.props.post.post_title,
+            postDescription: this.props.post.post_description,
+            createdAt: this.props.post.created_at,
+            updatedAt: this.props.post.updated_at,
+            numAttachments: this.props.post.attachments,
+            files: this.props.post.files
+        };
+    }
+
+    render() {
+        return(
+            <div>
+                <h3>Title: {this.state.postTitle}</h3>
+                <p>Description: {this.state.postDescription}</p>
+                <p>Number of attachments: {this.state.files.length}</p>
+                {this.state.files.map((file)=>{
+                    console.log('file: ', file);
+                    return <img src={`'http://127.0.0.1/${file.url}`} alt="image"  />
+                })}
+            </div>
+        )
+    }
+}
