1fd561bbea98985ab1068faffda3da0b1ae3375c
Muhammad Zeeshan Shafqat <zeeshan_github@outlook.com>
2021-03-18 16:09:48 +1100

    added login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
-import logo from './laravel-image.png';
-import Button from '@material-ui/core/Button';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from './components/Login';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import SignUp from './components/Signup';
+
 
 function App() {
+  let state = {
+    user: {},
+    token: null
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo}
-          alt="logo" 
-        />
-        <p>
-          Post-IT!!!!!
-        </p>
-        <Router>
-        <Button variant="contained" color="primary" >
-          <Link to="/login" style={{ textDecoration: 'none' }}>Login</Link>
-        </Button>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-        </Router>
-        
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/sign-in"}>Laravel-Task</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/log-in"}>Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">Log In with Google</a>
+                </li>              
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path="/log-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
