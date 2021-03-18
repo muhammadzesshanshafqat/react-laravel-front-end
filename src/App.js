43700a9f04d3a6f7911fd6441f2facedca3200de
Muhammad Zeeshan Shafqat <zeeshan_github@outlook.com>
2021-03-18 18:18:50 +1100

    Setup login

--- src/App.js
+++ src/App.js
@@ -1,49 +1,96 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { Component } from "react";
 import Login from './components/Login';
 import SignUp from './components/Signup';
 
 
-function App() {
-  let state = {
-    user: {},
-    token: null
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+      user: {
+        id: null,
+        email: null,
+        name: null,
+      },
+      token: null
+    }
+  
+
+    this.handleLogin = this.handleLogin.bind(this);
   }
-  return (
-    <Router>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/sign-in"}>Laravel-Task</Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/log-in"}>Login</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-                </li>
-                <li className="nav-item">
-                  <a  href={'http://127.0.0.1/api/auth/redirect'} className="nav-link">Log In with Google<i className="fab fa-google"></i></a>
-                </li>              
-              </ul>
+
+  componentDidMount() {
+    const userData = JSON.parse(localStorage.getItem('localUserData'));
+    if (userData) {
+        this.setState({
+            user: {
+              id: userData.user.id,
+              email: userData.user.email,
+              name: userData.user.name,
+            },
+            token: userData.token
+        });
+    } else {
+        this.setState({
+          user: {
+            id: null,
+            email: null,
+            name: null,
+          },
+          token: null
+        });
+    }
+  }
+
+  handleLogin(responseData) {
+    this.setState({
+      user: {
+        id: responseData.id,
+        email: responseData.email,
+        name: responseData.name
+      },
+      token: responseData.access_token 
+    });
+    localStorage.setItem('localUserData', JSON.stringify(this.state));
+  }
+  
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <Link className="navbar-brand" to={"/sign-in"}>Laravel-Task</Link>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/log-in"}>Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                  </li>
+                  <li className="nav-item">
+                    <a  href={'http://127.0.0.1/api/auth/redirect'} className="nav-link">Log In with Google<i className="fab fa-google"></i></a>
+                  </li>              
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Switch>
+                <Route exact path='/' component={Login} />
+                <Route exact path="/log-in"  render={(props) => <Login {...props} onLogin={this.handleLogin} />}/>
+                <Route exact path="/sign-up" component={SignUp} />
+              </Switch>
             </div>
           </div>
-        </nav>
-
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Switch>
-              <Route exact path='/' component={Login} />
-              <Route path="/log-in" component={Login} />
-              <Route path="/sign-up" component={SignUp} />
-            </Switch>
-          </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
-
-export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,66 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            rememberMe: false
+        };
+    
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleRememberMeChange = this.handleRememberMeChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleEmailChange(event) {
+        this.setState({email: event.target.value});
+    }
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
+    handleRememberMeChange(event) {
+        this.setState({rememberMe: event.target.checked});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('form was submitted: ', this.state, event);
+
+        axios.post('http://127.0.0.1/api/auth/login', {
+            email: this.state.email,
+            password: this.state.password,
+            remember_me: this.state.rememberMe
+          })
+          .then((response) => {
+            const responseData = response.data;
+            this.props.onLogin(responseData);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
             <h3>Log In</h3>
 
             <div className="form-group">
                 <label>Email address</label>
-                <input type="email" className="form-control" placeholder="Enter email" />
+                <input type="email" value={this.state.email} onChange={this.handleEmailChange} className="form-control" placeholder="Enter email" />
             </div>
 
             <div className="form-group">
                 <label>Password</label>
-                <input type="password" className="form-control" placeholder="Enter password" />
+                <input type="password" value={this.state.password} onChange={this.handlePasswordChange} className="form-control" placeholder="Enter password" />
             </div>
 
             <div className="form-group">
                 <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <input type="checkbox"  value={this.state.rememberMe} onChange={this.handleRememberMeChange} className="custom-control-input" id="customCheck1" />
                     <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                 </div>
             </div>
