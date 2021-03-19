af4d22a1a5270e6b203ee464639263f446b4cf35
Muhammad Zeeshan Shafqat <zeeshan_github@outlook.com>
2021-03-19 19:25:08 +1100

    added scroll to div

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -39,19 +39,15 @@ export default class Posts extends Component {
     render() {
         const noPosts = this.state.posts.length === 0;
         return(
-            <div>
-                <ul>
-                    {noPosts && <div>You do not have any posts.</div>}
-                    {this.state.posts.map((post, index) => {
-                        return <Post key={index} post={post}/>;
-                    })}
-                </ul>
-                {/* <button
-                    className="center-text btn btn-lg btn-danger center modal-button"
-                    onClick={this.showModal}
-                    >
-                    Create Post
-                </button> */}
+            <div style={{height: "100%"}}>
+                <div>
+                    <ul style={{overflowY:"scroll"}}>
+                        {noPosts && <div>You do not have any posts.</div>}
+                        {this.state.posts.map((post, index) => {
+                            return <Post key={index} post={post}/>;
+                        })}
+                    </ul>
+                </div>
             </div>
 
            
